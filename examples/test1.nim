a60301ae07deecdce9c11ecf9787dd17caf559b5
Vasiliy Stavenko <vstavenko@andiks.com>
2015-07-21 22:10:51 +0300

    consolidate glm

--- examples/test1.nim
+++ examples/test1.nim
@@ -1,16 +1,19 @@
 import glm
+import math
 
 proc main()=
     var 
         v1 = vec1()
         v2 = vec2()
         v3 = vec3(v2, 50.0)
+        m  = rotate(mat4(), vec3(1.0, 0, 0), math.PI/4)
     echo v3
     echo v1
     v1.x = 10.0
     echo v1
     v1[0] = 30
     echo v1
+    echo m, inverse(m)
 
 if isMainModule:
     main()
--- glm.nim
+++ glm.nim
@@ -1,3 +1,7 @@
 import glm.vec
+import glm.mat
+import glm.mat_transform
 
 export vec
+export mat
+export mat_transform
