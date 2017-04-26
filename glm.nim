6bbb6293838e8fed42b2bd52f9bc7deabd0bb940
Arne Döring <arne.doering@gmx.net>
2017-04-26 21:51:31 +0200

    spline

--- glm.nim
+++ glm.nim
@@ -1,9 +1,11 @@
-import glm.vec
-import glm.mat
-import glm.mat_transform
-import glm.quat
+import glm/vec
+import glm/mat
+import glm/mat_transform
+import glm/quat
+import glm/spline
 
 export vec
 export mat
 export mat_transform
 export quat
+export spline
