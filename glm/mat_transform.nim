5d1a091a00d5750b84c2a0c363ee5a59f0ca150e
CodeDoes <codedoes@gmail.com>
2018-07-12 07:18:26 +0200

    This is still not working need to add tests.

--- glm/mat_transform.nim
+++ glm/mat_transform.nim
@@ -93,16 +93,16 @@ proc scale*[T](m:Mat4[T], s: T): Mat4[T] {.inline.} =
   result.scaleInpl(s)
 
 ## DECOMPOSE
-proc pos*[T](m: Mat4[T]): var Vec3[T] =
-  m.row(0).xyz
+proc pos*[T](m: Mat4[T]): Vec3[T] =
+  return m.row(3).xyz
 proc `pos=`*[T](m: var Mat4[T], val: Vec3[T]) =
-  m.row0 = vec4f(val,1)
-proc scale*[T](m: Mat4[T]): var Vec3[T] =
-  m.diag.xyz
+  m.row3= vec4f(val,m.row(3)[3])
+proc scale*[T](m: Mat4[T]): Vec3[T] =
+  return m.diag.xyz
 proc scale*[T](m: var Mat4[T], val: Vec3[T]) =
   m.diag = vec4f(val,1)
-proc rot*[T](m: Mat4[T]): var Quat =
-  quat[T](m)
+proc rot*[T](m: Mat4[T]): Quat =
+  return quat(m)
 proc `rot=`*[T](m: var Mat4[T], val: Quat) =
   m = mat4[T]().transform(m.pos).scale(m.scale).rotate(val)
 ##TODO: SKEW, PERSPECTIVE
